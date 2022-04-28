2833e4b08d7b227f9b05b90cb889cd5dbabcf748
Hiroki-Fujishima <ucuc.as@gmail.com>
2022-04-29 02:48:49 +0900

    views修正

--- app_config/views.py
+++ app_config/views.py
@@ -1,8 +1,7 @@
-from urllib import request
-from django.shortcuts import render, redirect, reverse
-from django.views import View
+from django.shortcuts import render, redirect, reverse  
+from django.views import View  
 
 class index(View):
-    def (self, request, *args, **kwargs);
+    def get(self, request, *args, **kwargs):
         return redirect(reverse('app_folder:top_page'))
-index =  index.as_view()
+index = index.as_view()
--- app_folder/views.py
+++ app_folder/views.py
@@ -1,8 +1,7 @@
-import django
-from django.shortcuts import render
-form django.views import View
+from django.shortcuts import render  
+from django.views import View  
 
-class SampleView(View):
-    def get(self, request, *args, **kwargs):
+class SampleView(View):  
+    def get(self, request, *args, **kwargs):  
         return render(request, 'app_folder/top_page.html')
 top_page = SampleView.as_view()
